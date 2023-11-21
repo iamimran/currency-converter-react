010f7fb57447bb7db5436978c099171dbdecdea9
Imran Ahmed <iam.imran@yahoo.com>
2023-11-21 16:59:19 +0500

    attach events to InputBox components and also ID using useId react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl bg-orange-600 text-center">
         Currency Converter App
       </h1>
+      <InputBox />
     </>
   );
 }
